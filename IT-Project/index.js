1678197792ab090dc7ed55cb74bf6f4cd3b19233
Jayna Brisker <briskerjayna@gmail.com>
2026-02-04 16:40:01 -0800

    corrected comments of js file

--- IT-Project/index.js
+++ IT-Project/index.js
@@ -1,14 +1,16 @@
 document.addEventListener("click", function (event)
  {
-  // Make sure a table cell was clicked
+  // check if clicked cell tag has tag td,
+  //  if so then get parent element and define as row
   if (event.target.tagName === "TD") {
     const row = event.target.parentElement;
 
-    // Only toggle if the row has id="noreq"
+    // for rows with id "noreq" get background color of row
     if (row.id === "noreq") {
       const bg = window.getComputedStyle(row).backgroundColor;
 
-      // Toggle between white and green
+      // Toggles color if background is white
+      //  toggles color to white if green
       if (bg === "rgb(255, 255, 255)") {
         row.style.backgroundColor = "lightgreen";
       } else {
