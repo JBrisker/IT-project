b1de0d23ac143f7f1f699164eb9a3fba08f820f3
Jayna Brisker <briskerjayna@gmail.com>
2026-02-05 16:12:20 -0800

    replaced html table with javascript generated table. Added prereqs, and coreqs to table.

--- IT-Project/index.js
+++ IT-Project/index.js
@@ -1,21 +1,67 @@
-document.addEventListener("click", function (event)
- {
-  // check if clicked cell tag has tag td,
-  //  if so then get parent element and define as row
-  if (event.target.tagName === "TD") {
-    const row = event.target.parentElement;
+// data set for table
+const courses = [
+  
+  {id:1, name:"CSNT 115 A+ Software Essentials", credits:6, prereq:["noreq"], coreq:["CSNT 116"]},
+  {id:2,name:"CSNT 116 A+ Hardware", credits:6, prereq:["noreq"], coreq:["CSNT 115"]},
+  {id:3, name:"CMST& 210 Interpersonal Communication", credits:5, prereq:["noreq"]},
+  //Quarter 2
+  {id:4, name:"CSNT 124 Open Source Operating Systems", credits:6, prereq:["CSNT 115","CSNT 116"], coreq:["CSNT 125"]},
+  {id:5, name: "CSNT 125 Shells and Scripts", credits:6, prereq:["CSNT 115","CSNT 116"]},
+  {id:6, name:"CSD 110 Computer Programming Fundamentals with Python", credits:5, prereq:["noreq"]},
+  //Quarter 3
+  {id:7, name:"CSNT 231 Network Fundamentals I", credits:6, prereq:["CSNT 124","CSNT 125"]},
+  {id:8, name:"CSNT 232 Network Fundamentals II", credits:6, prereq:["CSNT 124","CSNT 125"]},
+  {id:9, name:"CS& 141 Computer Science I Java", credits:5, prereq:["CSD 110"]},
+  //Quarter 4
+  {id:10, name:"CJ& 101 Introduction to Criminal Justice", credits:5, prereq:["noreq"]},
+  {id:11, name:"ENGL& 101 English Composition I", credits:5, prereq:["noreq"]},
+  {id:12, name:"CS 143 Computer Science II Java", credits:5, prereq:["CS& 141"]},
+  //Quarter 5
+  {id:13, name:"MATH& 141", credits:5, prereq:["noreq"]},
+  {id:14, name:"CSNT 255 Network Administration I", credits:6, prereq:["CSNT 231","CSNT 232","ENGL& 101"]},
+  {id:15, name:"CSNT 256 Network Administration II", credits:6, prereq:["CSNT 231","CSNT 232"], coreq:["CSNT 255"]},
+  //Quarter6
+  {id:16, name:"MATH& 142", credits:5, prereq:["noreq"]},
+  {id:17, name:"CSNT 241 Network Security and Encryption", credits:6, prereq:["CSNT 255","CSNT 256"], coreq:["CSNT 248"]},
+  {id:18, name:"CSNT 248 Server Administration", credits:6, prereq:["CSNT 255","CSNT 256"], coreq:["CSNT 241"]},
+  //Quarter 7
+  {id:19, name:"CSNT 257 Cloud Computing", credits:6, prereq:["noreq"]},
+  {id:20, name:"CSNT 253 Capstone", credits:6, prereq:["CSNT 241","CSNT 248"], coreq:["CSNT 257"]},
+  {id:21, name:"MATH& 151 Calculus I", credits:5, prereq:["MATH& 142"]},
 
-    // for rows with id "noreq" get background color of row
-    if (row.id === "noreq") {
-      const bg = window.getComputedStyle(row).backgroundColor;
+];
+console.log(courses);
+const container = document.querySelector(".program");
 
-      // Toggles color if background is white
-      //  toggles color to white if green
-      if (bg === "rgb(255, 255, 255)") {
-        row.style.backgroundColor = "lightgreen";
-      } else {
-        row.style.backgroundColor = "white";
-      }
-    }
-  }
-});
+container.innerHTML = `
+  <table>
+    <thead>
+      <tr>
+        <th>Course ID</th>
+        <th>Course Code</th>
+        <th>Credits</th>
+        <th>Prereqs</th>
+        <th>Coreqs</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${courses.map(i => `
+        <tr>
+          <td>${i.id}</td>
+          <td>${i.name}</td>
+          <td>${i.credits ?? ""}</td>
+          <td>${i.prereq ? i.prereq.join(", ") : ""}</td>
+          <td>${i.coreq ? i.coreq.join(", ") : ""}</td>
+        </tr>
+      `).join("")}
+    </tbody>
+  </table>
+`;
+      
+
+
+
+
+
+
+
